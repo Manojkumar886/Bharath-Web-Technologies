98d032eefd1917fcdb8649f3261aa21360a24ce7
Manojkumar886 <maddymano434@gmail.com>
2023-05-03 11:56:45 +0530

    execution of functionnin js

---
+++ JavaScript/Functionusingaruguments.js
@@ -0,0 +1,19 @@
+function Execution()
+{
+    alert('A JavaScript function is a block of code designed to perform a particular task. A JavaScript function is executed when "something" invokes it (calls it).')
+}
+
+function Witharuguments(username,password)
+{
+    alert('username : '+username+" \n password : "+password )
+}
+
+let fatarrowExe1=()=>
+{
+    document.writeln("JavaScript Arrow Function · Before Arrow: · With Arrow Function: · Arrow Functions Return Value by Default: · Arrow Function With Parameters: · Arrow Function ")
+}
+
+var fatarrowExe2=(ab,ba)=>
+{
+    return ab/ba;
+}
